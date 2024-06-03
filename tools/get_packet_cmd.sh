dc5de9e18e3f67132341ee139e9b759c2b9bda98
Данил Голдышев <goldyshev02@mail.ru>
2024-06-03 21:17:39 +0300

    Обновление get_packet_cmd.sh
    Пока нет возможности полноценно протестировать работу системы на других
    дистрибутивах Linux, поэтому нет уверенности, что get_packet_cmd.sh содержит
    правильный набор библиотек.

--- tools/get_packet_cmd.sh
+++ tools/get_packet_cmd.sh
@@ -2,14 +2,14 @@
 
 case ${1} in
     Manjaro|Arch)
-        INSTALL_CMD="sudo pacman -S --needed qt5-base qt5-quickcontrols qt5-quickcontrols2 qt5-graphicaleffects qt5-remoteobjects qt5-declarative cmake make gcc git python"
+        INSTALL_CMD="sudo pacman -S --needed qt5-base qt5-quickcontrols qt5-quickcontrols2 qt5-remoteobjects qt5-declarative cmake make gcc git python"
         ;;
     Debian|Ubuntu|Astra)
-        INSTALL_CMD="sudo apt-get install -y qt5-default qt5-qmltooling-plugins qtdeclarative5-dev cmake make gcc git python3"
+        INSTALL_CMD="sudo apt-get install -y qt5-default qtbase5-private-dev qt5-qmltooling-plugins libqt5remoteobjects5 qtdeclarative5-dev cmake make gcc git python3"
         ;;
-    CentOS|Oracle)
-        INSTALL_CMD="sudo yum install -y qt5-qtbase qt5-qml qt5-qtdeclarative cmake make gcc git python3"
-        ;;
+    # CentOS|Oracle)
+    #     INSTALL_CMD="sudo yum install -y qt5-qtbase qt5-qml qt5-qtdeclarative cmake make gcc git python3"
+    #     ;;
     *)
         echo "Unsupported OS: $OS_DISTR_NAME"
         exit 1
