e90748fde83c7c27f5ee621e89c9b19def6edd59
Данил Голдышев <goldyshev02@mail.ru>
2024-06-03 22:54:47 +0300

    Обновление install.sh
    1)  Избавился от использования PATH: теперь устанавливаем символическую ссылку
        на QtAda в usr/local/bin/.
    2)  Весь вывод скрипта переношу в стандартный поток ошибок, чтобы не мешать
        получению нужных сообщений (см. пункт 3).
    3)  Добавил аргумент --obly-build, при котором установка/обновление
        символической ссылки не происходит, а результатом скрипта является
        путь к исполняемому файлу.

--- install.sh
+++ install.sh
@@ -6,15 +6,19 @@ YELLOW='\033[0;33m'
 NC='\033[0m'
 
 echo_info() {
-    echo -e "${GREEN}$1${NC}"
+    echo -e "${GREEN}$1${NC}" >&2
 }
 
 echo_warning() {
-    echo -e "${YELLOW}$1${NC}"
+    echo -e "${YELLOW}$1${NC}" >&2
 }
 
 echo_error() {
-    echo -e "${RED}$1${NC}"
+    echo -e "${RED}$1${NC}" >&2
+}
+
+quite_eval() {
+    eval $1 >&2
 }
 
 set -e
@@ -26,7 +30,7 @@ INSTALL_CMD=$(bash ./tools/get_packet_cmd.sh "${OS_NAME}")
 
 if [ -n "$INSTALL_CMD" ]; then
     echo_info "Installing necessary packages for $OS_NAME..."
-    eval $INSTALL_CMD
+    quite_eval "$INSTALL_CMD"
 else
     echo_error "Failed to get install command for $OS_NAME."
     exit 1
@@ -47,19 +51,24 @@ fi
 echo_info "Building the project..."
 mkdir -p build
 cd build
-cmake ..
-make
+quite_eval "cmake .."
+quite_eval "make"
 
 # TODO: По идее предыдущих команд достаточно
 # echo_info "Installing the application..."
 # sudo make install
 
 BIN_PATH=$(realpath $(pwd)/bin)
-echo_info "Adding the directory $BIN_PATH to PATH"
-echo "export PATH=\$PATH:$BIN_PATH" >> ~/.bashrc
-echo "export PATH=\$PATH:$BIN_PATH" >> ~/.bash_profile
-if [ -f ~/.zshrc ]; then
-    echo "export PATH=\$PATH:$BIN_PATH" >> ~/.zshrc
-fi
+APP_NAME=qtada
 
-echo_info "Installation completed. Please run 'source ~/.bashrc' to update your PATH."
+if [ "$1" == "--only-build" ]; then
+    echo "$BIN_PATH/$APP_NAME"
+else
+    LINK_PATH=/usr/local/bin/$APP_NAME
+    if [ -L $LINK_PATH ]; then
+        sudo rm $LINK_PATH
+    fi
+    quite_eval "sudo ln -s $BIN_PATH/$APP_NAME /usr/local/bin/$APP_NAME"
+    echo_info "Symbolic link created at '$LINK_PATH'. Call 'qtada' to run application"
+    echo_info "Installation completed."
+fi
